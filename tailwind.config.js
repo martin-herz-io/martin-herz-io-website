7b8bcdab026602eba2e82afa4e61b017ed6c9c6b
ImDinnerCat <mail@martin-herz.io>
2023-05-18 20:50:59 +0200

    [init] style settings & tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,16 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      'Josefin-Sans': ['Josefin Sans', 'sans-serif']
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '2.5rem',
+        '6xl': '3rem',
+        '7xl': '3.5rem',
+        '8xl': '4rem',
       },
     },
   },
