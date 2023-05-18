a1ab830b863cb41ad6ce15c64acf772157919f89
ImDinnerCat <mail@martin-herz.io>
2023-05-19 00:35:12 +0200

    [update] Responsive (mobile) design

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -19,12 +19,12 @@ export const Header: React.FC = () => {
     // HTML Return
     return (
         <div className={"width-full bg-zinc-900"}>
-            <div className={"container mx-auto py-8 flex items-center"}>
+            <div className={"container mx-auto py-8 flex flex-col md:flex-row items-center"}>
                 <div className={""}>
                     <h1 className={"text-2xl font-semibold mt-0.5 text-zinc-300"}>{name}</h1>
                 </div>
 
-                <div className={"ml-auto flex gap-3 items-center"}>
+                <div className={"md:ml-auto flex gap-3 items-center"}>
                     <a href={"https://www.linkedin.com/in/martin-andree-herz-21909823a/"} target={"_blank"} rel={"noopener noreferrer"} className={"text-md font-semibold text-zinc-500 hover:text-indigo-500 transition-all duration-300"}>
                         LinkedIn
                     </a>
