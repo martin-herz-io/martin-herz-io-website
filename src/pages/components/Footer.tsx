975ca690feb1b49f43dc6d0721272dee1cb44dfc
ImDinnerCat <mail@martin-herz.io>
2023-05-18 20:52:57 +0200

    [init] Footer component file

---
+++ src/pages/components/Footer.tsx
@@ -0,0 +1,12 @@
+// React imports
+import React from 'react';
+
+// Component
+export const Footer: React.FC = () => {
+
+    // HTML Return
+    return (
+        <div className={"w-full h-36 bg-zinc-800 mt-auto"}>
+        </div>
+    )
+}
