3f97907635677b306c14eeb0593d1f66aff24f92
ImDinnerCat <mail@martin-herz.io>
2023-05-19 03:04:37 +0200

    [update] RepositoryList Section

--- src/pages/components/RepositoryList.tsx
+++ src/pages/components/RepositoryList.tsx
@@ -1,5 +1,9 @@
 // React imports
 import React from 'react';
+import {Icon} from "./Icon";
+
+// Import components
+
 
 // Component
 export const RepositoryList: React.FC = () => {
@@ -7,36 +11,49 @@ export const RepositoryList: React.FC = () => {
     // Repository List from GitHub
     const [repositoryList, setRepositoryList] = React.useState<any[]>([])
 
-    // Fetch Repository List
+    // Fetch Repository List sort by last updated
     React.useEffect(() => {
-        fetch("https://api.github.com/users/martin-herz-io/repos")
+        fetch("https://api.github.com/users/martin-herz-io/repos?sort=updated")
             .then(response => response.json())
             .then(data => setRepositoryList(data))
     }, [])
 
     // HTML Return
     return (
-        <div className={"container mx-auto rounded-6xl flex flex-col items-center gap-4"}>
+        <div className={"container rounded-6xl flex flex-col items-center gap-4"}>
             <h2 className={"font-light text-lg"}>— Meine Repositories —</h2>
             <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full"}>
                 {repositoryList.map((repository, index) => {
                     return (
-                        <div key={index} className={"bg-zinc-800 rounded-2xl p-5 flex flex-col gap-2"}>
+                        <div key={index} className={"bg-zinc-800 rounded-3xl p-5 flex flex-col gap-3"}>
                             <div className={"flex flex-row items-center justify-between"}>
                                 <h3 className={"font-semibold text-xl text-zinc-300"}>{repository.name}</h3>
 
-                                <a href={repository.html_url} target={"_blank"} rel={"noopener noreferrer"} className={"w-fit text-md font-regular text-zinc-300 hover:text-indigo-500 transition-all duration-300"}>
-                                    Auf GitHub ansehen
+                                <a href={repository.html_url} target={"_blank"} rel={"noopener noreferrer"} className={"flex items-center gap-1 w-fit text-md font-regular text-zinc-300 hover:text-indigo-500 transition-all duration-300"}>
+                                    <Icon name={"logo-github"} className={"w-5 -translate-y-0.5"} />
+                                    GitHub
                                 </a>
                             </div>
 
                             <p className={"font-regular text-base text-zinc-400 mb-auto"}>{repository.description}</p>
 
-                            <div className={"flex flex-row gap-2"}>
+                            <div className={"flex flex-row flex-wrap gap-x-4 gap-y-2"}>
+                                <div className={"flex flex-row items-center gap-1"}>
+                                    <Icon name={"star"} className={"w-5 text-zinc-300"} />
+                                    <p className={"font-regular text-sm text-zinc-300"}>{repository.stargazers_count}</p>
+                                </div>
+
                                 {repository.language && (
                                     <div className={"flex flex-row items-center gap-1"}>
-                                        <div className={"w-3 h-3 rounded-full bg-zinc-300"}></div>
-                                        <p className={"font-regular text-base text-zinc-400"}>{repository.language}</p>
+                                        <Icon name={"code-slash"} className={"w-5 text-zinc-300"} />
+                                        <p className={"font-regular text-sm text-zinc-300"}>{repository.language}</p>
+                                    </div>
+                                )}
+
+                                {repository.license && (
+                                    <div className={"flex flex-row items-center gap-1"}>
+                                        <Icon name={"shield-checkmark"} className={"w-5 text-zinc-300"} />
+                                        <p className={"font-regular text-sm text-zinc-300"}>{repository.license.spdx_id}</p>
                                     </div>
                                 )}
                             </div>
