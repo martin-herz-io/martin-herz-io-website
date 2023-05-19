ec8a65c7046112f52306fffd313f18713ea0a15e
ImDinnerCat <mail@martin-herz.io>
2023-05-19 03:44:11 +0200

    [update] Add saturate & opacity hover effect

--- src/pages/components/LogoCarousel.tsx
+++ src/pages/components/LogoCarousel.tsx
@@ -14,19 +14,19 @@ export const LogoCarousel: React.FC<Props> = ({title, logos}) => {
     return (
         <div className={"md:container flex flex-col items-center gap-4"}>
             <h2 className={"font-light text-lg"}>{`— ${title} —`}</h2>
-            <div className={"w-full bg-zinc-800 py-12 md:py-16 md:rounded-full marquee-hover-pause overflow-hidden"}>
-                <div className={"marquee opacity-50"}>
+            <div className={"group w-full bg-zinc-800 py-12 md:py-16 md:rounded-full marquee-hover-pause overflow-hidden"}>
+                <div className={"marquee opacity-50 saturate-0 group-hover:opacity-100 group-hover:saturate-100 transition duration-300"}>
                     <div className={"marquee-content"}>
                         {logos.map((logo, index) => {
                             return (
-                                <img key={index} alt={logo} src={logo} className={"h-6 md:h-8 saturate-0"} />
+                                <img key={index} alt={logo} src={logo} className={"h-6 md:h-8"} />
                             )
                         })}
                     </div>
                     <div className={"marquee-content"} aria-hidden={true}>
                         {logos.map((logo, index) => {
                             return (
-                                <img key={index} alt={logo} src={logo} className={"h-6 md:h-8 saturate-0"} />
+                                <img key={index} alt={logo} src={logo} className={"h-6 md:h-8"} />
                             )
                         })}
                     </div>
