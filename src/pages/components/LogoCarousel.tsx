8aa7e4e2883aacecf2f16517e68ad3a2c89a7a05
ImDinnerCat <mail@martin-herz.io>
2023-05-19 03:04:01 +0200

    [add 2.0] LogoCarousel section

---
+++ src/pages/components/LogoCarousel.tsx
@@ -0,0 +1,37 @@
+// React imports
+import React from 'react';
+
+// Properties
+export type Props = {
+    title: string,
+    logos: string[],
+}
+
+// Component
+export const LogoCarousel: React.FC<Props> = ({title, logos}) => {
+
+    // HTML Return
+    return (
+        <div className={"md:container flex flex-col items-center gap-4"}>
+            <h2 className={"font-light text-lg"}>{`— ${title} —`}</h2>
+            <div className={"w-full bg-zinc-800 py-12 md:py-16 md:rounded-full marquee-hover-pause overflow-hidden"}>
+                <div className={"marquee opacity-50"}>
+                    <div className={"marquee-content"}>
+                        {logos.map((logo, index) => {
+                            return (
+                                <img src={logo} className={"h-6 md:h-8"} />
+                            )
+                        })}
+                    </div>
+                    <div className={"marquee-content"} aria-hidden={true}>
+                        {logos.map((logo, index) => {
+                            return (
+                                <img src={logo} className={"h-6 md:h-8"} />
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
