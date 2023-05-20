034cfd14d1f598a8f11cd5d75dd776c502d354c4
ImDinnerCat <mail@martin-herz.io>
2023-05-20 09:04:55 +0200

    [update] Add React to knowledge list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 
 // Component imports
-import {Header} from "./components/Header";
-import {Footer} from "./components/Footer";
-import {IntroSection} from "./components/IntroSection";
-import {RepositoryList} from "./components/RepositoryList";
-import {LogoCarousel} from "./components/LogoCarousel";
+import {Header} from "../components/Header";
+import {Footer} from "../components/Footer";
+import {IntroSection} from "../components/IntroSection";
+import {RepositoryList} from "../components/RepositoryList";
+import {LogoCarousel} from "../components/LogoCarousel";
 
 const Home: NextPage = () => {
 
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
                 <title>{`Martin-Andree Herz`}</title>
             </Head>
 
-            <main className={"flex flex-col gap-16 md:gap-32 min-h-screen"}>
+            <main className={"flex flex-col gap-16 md:gap-40 min-h-screen"}>
 
                 <Header/>
 
@@ -36,6 +36,7 @@ const Home: NextPage = () => {
                     title={"Meine Kenntnisse"}
                     logos={[
                         "/knowledge/typescript.png",
+                        "/knowledge/react.png",
                         "/knowledge/nextjs.png",
                         "/knowledge/nodejs.png",
                         "/knowledge/mariadb.png",
