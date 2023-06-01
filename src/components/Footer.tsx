598e9dbe36b36cf6b8241e8b0d9db077959ec709
ImDinnerCat <mail@martin-herz.io>
2023-06-01 21:57:02 +0200

    [update] footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,10 @@ export const Footer: React.FC = () => {
 
     // HTML Return
     return (
-        <div className={"w-full h-36 bg-zinc-800 mt-auto"}>
+        <div className={"w-full bg-zinc-800 mt-auto text-center py-6"}>
+            <p className={"font-light opacity-60"}>
+                © Copyright 2023 Martin-Andree Herz - All Rights Reserved.
+            </p>
         </div>
     )
 }
